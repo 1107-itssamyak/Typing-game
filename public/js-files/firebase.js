ee87c632efebf8a8a47e3bb69c6052723d00aa7a
1107-itssamyak <samyak.mehta11@gmail.com>
2021-10-27 18:29:45 +0530

    fix security issue

--- public/js-files/firebase.js
+++ public/js-files/firebase.js
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-analytics.js";
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBR8dFJEFP7HpS3ebTsZ4hO3HoFr2xSmvc",
-    authDomain: "typing-game-a1.firebaseapp.com",
-    projectId: "typing-game-a1",
-    storageBucket: "typing-game-a1.appspot.com",
-    messagingSenderId: "175761880903",
-    appId: "1:175761880903:web:ed9badcc99e72ea0063e62",
-    measurementId: "G-WW8SKMYWGQ"
-};
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: "AIzaSyA3XqYoMTpqdJzhoiGoOBNONShETmcQkds",
+//     authDomain: "typing-game-1.firebaseapp.com",
+//     projectId: "typing-game-1",
+//     storageBucket: "typing-game-1.appspot.com",
+//     messagingSenderId: "830250800316",
+//     appId: "1:830250800316:web:15b224bfcabe46272f6cb9",
+//     measurementId: "G-9BFR63RPCJ"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
-export default app;
+// export default app;
