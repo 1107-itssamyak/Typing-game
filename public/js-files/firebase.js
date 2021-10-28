d0df2bfcd1f5b451ae5e103e8e65e0406d2f7c71
1107-itssamyak <samyak.mehta11@gmail.com>
2021-10-28 10:12:07 +0530

    fix and UI updates for login and gamepage

--- public/js-files/firebase.js
+++ public/js-files/firebase.js
@@ -1,23 +1,23 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//     apiKey: "AIzaSyA3XqYoMTpqdJzhoiGoOBNONShETmcQkds",
-//     authDomain: "typing-game-1.firebaseapp.com",
-//     projectId: "typing-game-1",
-//     storageBucket: "typing-game-1.appspot.com",
-//     messagingSenderId: "830250800316",
-//     appId: "1:830250800316:web:15b224bfcabe46272f6cb9",
-//     measurementId: "G-9BFR63RPCJ"
-// };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA3XqYoMTpqdJzhoiGoOBNONShETmcQkds",
+    authDomain: "typing-game-1.firebaseapp.com",
+    projectId: "typing-game-1",
+    storageBucket: "typing-game-1.appspot.com",
+    messagingSenderId: "830250800316",
+    appId: "1:830250800316:web:15b224bfcabe46272f6cb9",
+    measurementId: "G-9BFR63RPCJ"
+};
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
-// export default app;
+export default app;
